759e51df99f91e8c673ae1807bd219d4d8582aef
FrandyAquino <frandyjavieraquino13@gmail.com>
2024-09-10 17:42:31 -0400

    Agregando estilos  y modificaciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import styles from './App.module.css';
 import { useState } from 'react';
+import { FaCloud, FaTint, FaWind, FaThermometerHalf, FaMapMarkerAlt, FaGithub, FaLinkedin, FaLocationArrow, FaCloudscale  } from 'react-icons/fa';
 
 interface WeatherData {
-  name: string;
-  weather: { description: string }[];
+  coord: { lon: number, lat: number };
+  weather: { id: number, main: string, description: string, icon: string }[];
   main: {
     temp: number;
+    feels_like: number;
+    pressure: number;
     humidity: number;
+    temp_min: number;
+    temp_max: number;
+    sea_level: number;
+    grnd_level: number;
   };
   wind: {
     speed: number;
+    deg: number;
+    gust: number;
   };
+  clouds: { all: number };
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  name: string;
 }
 
 function App() {
@@ -41,36 +58,80 @@ function App() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.header}>Weather App</h1>
-      <div className={styles.grid}>
-        <div className={styles.card}>
+      <nav className={styles.navbar}>
+        <div className={styles.navContent}>
+          <h1 className={styles.header}>WeatherApp</h1>
           <input
-            className={styles.input}
+            className={styles.searchBar}
             type="text"
-            placeholder="Ingresa una ciudad"
+            placeholder="Buscar ciudad..."
             value={city}
             onChange={handleChange}
           />
           <button className={styles.button} onClick={fetchData}>Consultar</button>
         </div>
+      </nav>
+
+      <div className={styles.grid}>
         {weatherData && (
-          <div className={`${styles.card} ${styles.mainCard}`}>
-            <h2 className={styles.cityName}>{weatherData.name}</h2>
-            <div className={styles.mainTemp}>
-              <div className={styles.mainIcon}>
+          <>
+            <div className={`${styles.card} ${styles.mainCard}`}>
+              <h2 className={styles.cityName}>
+                <FaMapMarkerAlt /> {weatherData.name}, {weatherData.sys.country}
+              </h2>
+              <div className={styles.mainTemp}>
+                <div className={styles.mainIcon}>
+                  <FaCloud />
+                </div>
+                <div className={styles.tempValue}>
+                  {weatherData.main.temp} °C
+                </div>
               </div>
-              <div className={styles.tempValue}>
-                {weatherData.main.temp} °C
+              <p className={styles.weatherDesc}>
+                {capitalizeWords(weatherData.weather[0].description)}
+              </p>
+            </div>
+            <div className={styles.bentoGrid}>
+              <div className={styles.bentoCard}>
+                <FaTint className={styles.icon} />
+                <p className={styles.value}>Humedad: {weatherData.main.humidity}%</p>
+              </div>
+              <div className={styles.bentoCard}>
+                <FaThermometerHalf className={styles.icon} />
+                <p className={styles.value}>Sensación térmica: {weatherData.main.feels_like} °C</p>
+              </div>
+              <div className={styles.bentoCard}>
+                <FaWind className={styles.icon} />
+                <p className={styles.value}>Viento: {weatherData.wind.speed} m/s</p>
+              </div>
+              <div className={styles.bentoCard}>
+                <FaLocationArrow className={styles.icon} />
+                <p className={styles.value}>Latitud: {weatherData.coord.lat}</p>
+                <p className={styles.value}>Longitud: {weatherData.coord.lon}</p>
+              </div>
+              <div className={styles.bentoCard}>
+                <FaCloudscale className={styles.icon} />
+                <p className={styles.value}>Presión: {weatherData.main.pressure} hPa</p>
+              </div>
+              <div className={styles.bentoCard}>
+                <FaCloud className={styles.icon} />
+                <p className={styles.value}>Nubes: {weatherData.clouds.all}%</p>
               </div>
             </div>
-            <p className={styles.weatherDesc}>
-              {capitalizeWords(weatherData.weather[0].description)}
-            </p>
-            <p className={styles.value}>Humedad: {weatherData.main.humidity}%</p>
-            <p className={styles.value}>Velocidad del viento: {weatherData.wind.speed} m/s</p>
-          </div>
+          </>
         )}
       </div>
+      <footer className={styles.footer}>
+        <p>Desarrollado por Frandy Aquino</p>
+        <div className={styles.socialLinks}>
+          <a href="https://github.com/FrandyAquino" target="_blank" rel="noopener noreferrer">
+            <FaGithub className={styles.socialIcon} />
+          </a>
+          <a href="https://www.linkedin.com/in/frandyaquino/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className={styles.socialIcon} />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
